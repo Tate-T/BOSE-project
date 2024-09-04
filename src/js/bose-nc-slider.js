8b1c5e98db60af011efe77e075f6cb35d8e68bde
DimaTurkey <123176664+DimaTurkey@users.noreply.github.com>
2024-09-04 15:53:39 +0300

    acordeon-slider-ended

--- src/js/bose-nc-slider.js
+++ src/js/bose-nc-slider.js
@@ -1,46 +1,36 @@
-// let nextBtn = document.querySelector("#next");
-// let prevBtn = document.querySelector("#prev");
-// let slides = document.querySelectorAll(".slide");
-// let changeSlide = 0;
-// // console.log(changeSlide);
-// nextBtn.addEventListener("click", function() {
-  
-//     slides.forEach(function (slide, index) {
-//     if (slide.classList.contains("show") === true) {
-//       changeSlide = index + 1;
-//       slide.classList.remove("show");
-//     }
-    
-//   });
-// //   console.log(changeSlide);
-//   if (changeSlide < slides.length) {
-//     slides[changeSlide].classList.add("show");
-//     }
-//   else {
-//       changeSlide = 0;
-//       slides[changeSlide].classList.add("show");
-//     }
-// });
-// // console.log(changeSlide);
-// prevBtn.addEventListener('click', function () {
-   
-//     slides.forEach(function (slide, index) {
-//         if (slide.classList.contains("show") === true) {
-//             changeSlide = index - 1;
-//             slide.classList.remove("show");
-//         }
-       
-        
-//     });
-//     // console.log(changeSlide);
+let items = document.querySelectorAll('.slider .slide');
+let active = 0;
+let lengthitems = items.length - 1;
+let autoSlide; 
 
-//     if (changeSlide < slides.length && changeSlide > -1) {
-//         slides[changeSlide].classList.add("show");
-//     }
-//     else {
-//         // console.log(slides.length);
-        
-//         changeSlide = slides.length - 1;
-//         slides[changeSlide].classList.add("show");
-//     }
-// });
+function nextSlide() {
+    if (active + 1 > lengthitems) {
+        active = 0;
+    } else {
+        active += 1;
+    }
+    reloadSlider();
+}
+
+function reloadSlider() {
+    items.forEach(slide => slide.classList.remove("show"));
+    items[active].classList.add("show");
+}
+
+function startAutoSlide() {
+    autoSlide = setInterval(nextSlide, 14000); 
+}
+
+function stopAutoSlide() {
+    clearInterval(autoSlide); 
+}
+
+startAutoSlide();
+
+
+let iframes = document.querySelectorAll('.reviews__video');
+iframes.forEach(iframe => {
+    iframe.addEventListener('play', stopAutoSlide);
+    iframe.addEventListener('pause', startAutoSlide);
+    iframe.addEventListener('ended', startAutoSlide);
+});
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,46 +1,20 @@
-let nextBtn = document.querySelector("#next");
-let prevBtn = document.querySelector("#prev");
-let slides = document.querySelectorAll(".slide");
-let changeSlide = 0;
-// console.log(changeSlide);
-nextBtn.addEventListener("click", function() {
-  
-    slides.forEach(function (slide, index) {
-    if (slide.classList.contains("show") === true) {
-      changeSlide = index + 1;
-      slide.classList.remove("show");
-    }
-    
-  });
-//   console.log(changeSlide);
-  if (changeSlide < slides.length) {
-    slides[changeSlide].classList.add("show");
-    }
-  else {
-      changeSlide = 0;
-      slides[changeSlide].classList.add("show");
-    }
-});
-// console.log(changeSlide);
-prevBtn.addEventListener('click', function () {
-   
-    slides.forEach(function (slide, index) {
-        if (slide.classList.contains("show") === true) {
-            changeSlide = index - 1;
-            slide.classList.remove("show");
-        }
-       
-        
-    });
-    // console.log(changeSlide);
+let items = document.querySelectorAll('.slider .list .item');
+let active = 0;
+let lengthitems = items.length - 1;
 
-    if (changeSlide < slides.length && changeSlide > -1) {
-        slides[changeSlide].classList.add("show");
+function nextSlide() {
+    if (active + 1 > lengthitems) {
+        active = 0;
+    } else {
+        active = active + 1;
     }
-    else {
-        // console.log(slides.length);
-        
-        changeSlide = slides.length - 1;
-        slides[changeSlide].classList.add("show");
-    }
-});
+    reloadslider();
+}
+
+function reloadslider() {
+    items.forEach(slide => slide.classList.remove("show"));
+    items[active].classList.add("show");
+}
+
+// Автоматичне перемикання кожні 7 секунд
+let autoslide = setInterval(nextSlide, 14000);
